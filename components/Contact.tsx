26ce1197a749a19f84690a33f84b155d0aaf6729
david temitope <temi236topemicheal.com>
2023-08-20 08:06:51 +0100

    Work on UI design for portfolio website

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -42,9 +42,9 @@ const Contact = (props: Props) => {
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2  w-fit mx-auto"
+          className="flex flex-col space-y-2  w-[80%] lg:w-fit mx-auto"
         >
-          <div className="flex space-x-2">
+          <div className="flex gap-x-2 flex-col lg:flex-row">
             <input
               {...register("name")}
               placeholder="Name"
