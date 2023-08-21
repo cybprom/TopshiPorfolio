fb1564db7b3ad15b5a5a60a983cdc9bbd03aae54
david temitope <temi236topmicheal@gmail.com>
2023-08-21 06:25:10 +0100

    sanity update

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+// import {defineConfig} from 'sanity'
+// import {deskTool} from 'sanity/desk'
+// import {visionTool} from '@sanity/vision'
+// import {schemaTypes} from './schemas'
 
-export default defineConfig({
-  name: 'default',
-  title: 'portfolio site',
+// export default defineConfig({
+//   name: 'default',
+//   title: 'portfolio site',
 
-  projectId: 'xu1ilqgm',
-  dataset: 'production',
+//   projectId: 'xu1ilqgm',
+//   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+//   plugins: [deskTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
-  },
-})
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
