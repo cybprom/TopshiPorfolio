6b0608bf2b006f4d921cc3e03dcdbfcfea49926b
david temitope <temi236topmicheal@gmail.com>
2023-08-22 11:46:19 +0100

    testing api

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,28 +1,11 @@
 import { pageInfo } from "./../typings";
 
 export const fetchPageInfo = async () => {
-  try {
-    const res = await fetch(
-      `${
-        process.env.NEXT_PUBLIC_BASE_URL ||
-        "https://temitopedavidsite.vercel.app"
-      }/api/getPageInfo`
-    );
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
+  );
+  const data = await res.json();
+  const pageInfo: pageInfo = data.pageInfo;
 
-    if (!res.ok) {
-      throw new Error(`API request failed with status: ${res.status}`);
-    }
-
-    const data = await res.json();
-    const pageInfo: pageInfo = data.pageInfo;
-
-    return pageInfo;
-    console.log(data);
-  } catch (error) {
-    console.error("Error fetching page info:", error);
-    // You can also throw the error here to let the caller handle it
-    throw error;
-  }
+  return pageInfo;
 };
-
-fetchPageInfo();
