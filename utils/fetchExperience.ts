38b08fc6971e6214b31d13422cf47a9e9e95b616
david temitope <temi236topmicheal@gmail.com>
2023-08-22 10:24:55 +0100

    testing api

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -9,5 +9,3 @@ export const fetchExperience = async () => {
 
   return experience;
 };
-
-fetchExperience();
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -6,7 +6,7 @@ export const fetchProjects = async () => {
   );
   const data = await res.json();
   const projects: Project[] = data.project;
-  console.log(projects);
+
   return projects;
 };
 
