56fcbed558cc4244875f0d28f4d485fda932e2e3
david temitope <temi236topmicheal@gmail.com>
2023-08-23 05:11:52 +0100

    Refactor of code to app router

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const nextConfig = {}
 
-module.exports = {
-  images: {
-    domains: [
-      "www.google.com",
-      "encrypted-tbn0.gstatic.com",
-      "cdn.sanity.io",
-      "localhost",
-    ], // Add the necessary domains here
-  },
-};
+module.exports = nextConfig
--- sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import experience from './experience'
-import pageInfo from './pageInfo'
-import project from './project'
-import skills from './skills'
-import socials from './socials'
-
-export const schemaTypes = [pageInfo, experience, socials, project, skills]
