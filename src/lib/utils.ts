9b918b7886c8b5a7abd88750904d16e9dd48eb10
david temitope <temi236topmicheal@gmail.com>
2023-08-23 13:20:58 +0100

    converting to app router

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import imageUrlBuilder from "@sanity/image-url";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
