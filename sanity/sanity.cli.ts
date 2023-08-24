aa62cacbac1af1517ce39c91f4b1bb0198fceb6c
david temitope <temi236topmicheal@gmail.com>
2023-08-24 20:33:33 +0100

    update

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'xu1ilqgm',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
